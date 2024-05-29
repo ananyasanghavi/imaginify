278157cafcebea821adaa612efab920b8d46cb26
ananyasanghavi <114865361+ananyasanghavi@users.noreply.github.com>
2024-05-29 14:21:07 +0530

    fix  clerk webhooks type

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,9 +4,9 @@
 declare type CreateUserParams = {
   clerkId: string;
   email: string;
-  username: string | null;
-  firstName: string | null;
-  lastName: string | null;
+  username: string | "";
+  firstName: string | "";
+  lastName: string | "";
   photo: string;
 };
 
