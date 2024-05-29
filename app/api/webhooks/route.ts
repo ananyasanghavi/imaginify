9a4614c826361135c5fd495b23dc10cad0f7755a
ananyasanghavi <114865361+ananyasanghavi@users.noreply.github.com>
2024-05-29 14:06:43 +0530

    fix  clerk webhooks type

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username,
+      username: username || "",
       firstName: first_name || "",
       lastName: last_name || "",
       photo: image_url,
@@ -89,9 +89,9 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
-      username: username!,
+      firstName: first_name || "",
+      lastName: last_name || "",
+      username: username || "",
       photo: image_url,
     };
 
